6aa6096a296d615bab4cc4268ded3c42082d1c99
Takafumi Arakaki <aka.tkf@gmail.com>
2020-07-24 19:20:40 -0700

    Fix for Julia 1.0

--- test/test_misc.jl
+++ test/test_misc.jl
@@ -17,9 +17,19 @@ end
 @testset "@evaltest_throw success" begin
     script = joinpath(@__DIR__, "exec_evaltest_throw_success.jl")
     cmd = `$(Base.julia_cmd()) --startup-file=no $script`
-    io = IOBuffer()
-    proc = run(pipeline(ignorestatus(cmd), stdout = io, stderr = io, stdin = devnull))
-    output = String(take!(io))
+    if VERSION < v"1.1"
+        out = Pipe()
+        reader = @async begin
+            read(out, String)
+        end
+        proc = run(pipeline(ignorestatus(cmd), stdout = out, stderr = out, stdin = devnull))
+        close(out.in)
+        output = fetch(reader)
+    else
+        io = IOBuffer()
+        proc = run(pipeline(ignorestatus(cmd), stdout = io, stderr = io, stdin = devnull))
+        output = String(take!(io))
+    end
     @test proc.exitcode != 0
     @test occursin("Test Failed", output)
 end
